50657e107d7f93a610fc4e4f36a5fe2c8d2266e9
liuyue <liuyue@soyoung.com>
2022-01-13 14:34:20 +0800

    todoList全局事件总线

---
+++ 06 todoList_全局事件总线/components/Myfooter.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="todo-footer" v-show="total">
+    <label>
+      <!-- <input type="checkbox" :checked="isAll" @change="checkAll"/> -->
+			<input type="checkbox" v-model="isAll"/>
+    </label>
+    <span>
+      <span>已完成{{doneTotal}}</span> / {{total}}
+    </span>
+    <button class="btn btn-danger" @click="clickAll">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Myfooter',
+	props: ['todos'],
+	computed: {
+		// 全部数量
+		total(){
+			return this.todos.length
+		},
+		// 已完成数量
+		doneTotal(){
+			return this.todos.reduce((pre,todo)=> pre + (todo.done ? 1 : 0) ,0)
+		},
+		// 删除已完成任务
+		// isAll(){
+		// 	return this.doneTotal === this.total && this.total >0
+		// }
+		
+		// 删除已完成任务和已完成按钮
+		isAll:{
+			get(){
+				return this.doneTotal === this.total && this.total >0
+			},
+			set(value){
+				// this.checkAllTodo(value)
+				this.$emit('checkAllTodo', value)
+			}
+		}
+	},
+	methods: {
+		// 已完成按钮
+		// checkAll(e){
+		// 	// console.log(e.target.checked)
+		// 	this.checkAllTodo(e.target.checked)
+		// }
+		clickAll(){
+			// this.clearAllTodo()
+			this.$emit('clearAllTodo')
+		}
+	}
+}
+</script>
+
+<style scoped>
+	/*footer*/
+	.todo-footer {
+		height: 40px;
+		line-height: 40px;
+		padding-left: 6px;
+		margin-top: 5px;
+	}
+
+	.todo-footer label {
+		display: inline-block;
+		margin-right: 20px;
+		cursor: pointer;
+	}
+
+	.todo-footer label input {
+		position: relative;
+		top: -1px;
+		vertical-align: middle;
+		margin-right: 5px;
+	}
+
+	.todo-footer button {
+		float: right;
+		margin-top: 5px;
+	}
+</style>
---
+++ 06 todoList_全局事件总线/components/Mylist.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="todo-main">
+    <Myitem 
+			v-for="todoObjj in todo" 
+			:key="todoObjj.id" 
+			:todo="todoObjj"/>
+  </ul>
+</template>
+
+<script>
+import Myitem from './Myitem.vue'
+export default {
+  name: 'Mylist',
+  components: { Myitem },
+	props:["todo"],
+}
+</script>
+
+<style scoped>
+	/*main*/
+	.todo-main {
+		margin-left: 0px;
+		border: 1px solid #ddd;
+		border-radius: 2px;
+		padding: 0px;
+	}
+
+	.todo-empty {
+		height: 40px;
+		line-height: 40px;
+		border: 1px solid #ddd;
+		border-radius: 2px;
+		padding-left: 5px;
+		margin-top: 10px;
+	}
+</style>
